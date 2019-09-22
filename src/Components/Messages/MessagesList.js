ce0c09394b82fdcccf563853cdede0dc1f0d09e4
Temitope <temmieayodele@gmail.com>
2019-09-22 19:40:52 +0100

    added a new div

--- src/Components/Messages/MessagesList.js
+++ src/Components/Messages/MessagesList.js
@@ -1,28 +1,29 @@
 import React from "react"
 
-const MessagesList = ({employees}) =>{
+const MessagesList = ({ employees }) => {
     return (
         <div className="messages-list">
-            {employees.map((employee)=>{
-                return(
+            {employees.map((employee) => {
+                return (
                     <div key={employee.id} className="employee">
-                        <section className="checkbox">
+                        <div className="wrapper">
+                            <section className="checkbox">
                                 <input type="checkbox" name="employee" className="checkbox" />
-                        </section>
-                        <section>
-                            <img src={employee.avatar_url} alt="img"/>
-                        </section>
-                        <section className="sender">
-                            <span>{employee.login}</span>
-                        </section>
-                        <section className="message-details">
-                            <span className="category">{employee.category}</span>
-                            <span className="message-info">{employee.message_info}</span>
-                        </section>
-                        <section>
-                            <span className="ellipsis"><i className="fas fa-ellipsis-v" /></span>
-                        </section>
-
+                            </section>
+                            <section>
+                                <img src={employee.avatar_url} alt="img" />
+                            </section>
+                            <section className="sender">
+                                <span>{employee.login}</span>
+                            </section>
+                            <section className="message-details">
+                                <span className="category">{employee.category}</span>
+                                <span className="message-info">{employee.message_info}</span>
+                            </section>
+                            <section>
+                                <span className="ellipsis"><i className="fas fa-ellipsis-v" /></span>
+                            </section>
+                        </div>
 
                     </div>
                 )
