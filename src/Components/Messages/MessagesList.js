88612ca410a8916a966a7f04d94387554cc987e5
Temitope <temmieayodele@gmail.com>
2019-09-22 10:59:20 +0100

    added new features

--- src/Components/Messages/MessagesList.js
+++ src/Components/Messages/MessagesList.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const MessagesList = ({employees}) =>{
     return (
-        <div>
+        <div className="messages-list">
             {employees.map((employee)=>{
                 return(
                     <div key={employee.id} className="employee">
@@ -12,15 +12,15 @@ const MessagesList = ({employees}) =>{
                         <section>
                             <img src={employee.avatar_url} alt="img"/>
                         </section>
-                        <section>
+                        <section className="sender">
                             <span>{employee.login}</span>
                         </section>
-                        <section>
-                            <span>{employee.category}</span>
-                            <span>{employee.message_info}</span>
+                        <section className="message-details">
+                            <span className="category">{employee.category}</span>
+                            <span className="message-info">{employee.message_info}</span>
                         </section>
                         <section>
-                            <span>...</span>
+                            <span className="ellipsis"><i className="fas fa-ellipsis-v" /></span>
                         </section>
 
 
